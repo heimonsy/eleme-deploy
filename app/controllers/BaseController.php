6e7b6ec701578401a7fc820aa8e69569f555331a
heimonsy <heimonsy@gmail.com>
2014-11-14 18:18:43 +0800

    add base permission control

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -14,11 +14,28 @@ class BaseController extends Controller
         $sites = (new WebSite())->getList();
         $user = GithubLogin::getLoginUser();
         $validSites = array();
+        $adminSites = array();
         foreach ($sites as $m) {
             if (!empty($user->permissions[$m['siteId']])) {
                 $validSites[] = $m;
+                if (DeployPermissions::havePermission(DeployPermissions::WRITE, $user->permissions[$m['siteId']])) {
+                    $adminSites[] = $m['siteId'];
+                }
             }
         }
+
+        $isSuperUser = false;
+        $i = 0;
+        while (!empty($_ENV["SUPER_USERS.{$i}"])) {
+            if ($_ENV["SUPER_USERS.{$i}"] == $user->login) {
+                $isSuperUser = true;
+                break;
+            }
+            $i++;
+        }
+
         View::share('sites', $validSites);
+        View::share('isSuperUser', $isSuperUser);
+        View::share('adminSites', $adminSites);
     }
 }
