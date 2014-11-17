ab5a25f1a83ddc9071c0b5b74f7005950d223c08
heimonsy <heimonsy@gmail.com>
2014-11-17 18:47:24 +0800

    fix checkout bug

--- app/jobs/PullRequestBuild.php
+++ app/jobs/PullRequestBuild.php
@@ -20,6 +20,7 @@ class PullRequestBuild
         $repoName = $commitInfo->repo;
         $gitOrigin = "git@github.com:{$repoName}.git";
         $dc = new DC($siteId);
+        $branch = $commitInfo->branch;
 
         $root = (new SystemConfig())->get(SystemConfig::WORK_ROOT_FIELD) . '/' . $siteId;
 
@@ -50,7 +51,7 @@ class PullRequestBuild
             }
 
             Log::info("git fetch origin");
-            $cmd = "git fetch origin --depth 10";
+            $cmd = "git fetch origin $branch:$branch --depth 20";
             (new Process($cmd, $branchRoot))->setTimeout(600)->mustRun();
 
             if (File::exists($commitPath)) {
@@ -61,6 +62,10 @@ class PullRequestBuild
             $cmd = "cp -r $branchRoot $commitPath";
             Log::info($cmd);
             (new Process($cmd))->setTimeout(600)->mustRun();
+
+            $cmd = "git checkout {$branch}";
+            Log::info($cmd);
+            (new Process($cmd, $commitPath))->mustRun();
 
             $cmd = "git checkout {$commit}";
             Log::info($cmd);
