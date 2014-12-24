d5c964e486965320259c0c0e36e3dd331ccd1f9d
heimonsy <heimonsy@gmail.com>
2014-12-24 15:21:20 +0800

    add mail configure and env sample

---
+++ .env.sample.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return array(
+    'GITHUB_ORGANIZATION' => '',
+    'HOST_URL' => '',
+
+    'GITHUB_CLIENT_ID' => '',
+    'GITHUB_CLIENT_SECRET' => '',
+    'CLIENT_PROXY' => '',
+
+    'SUPER_USERS' => array(),
+
+    'SMTP_HOST' => '',
+    'SMTP_PORT' => 222,
+    'SMTP_ENCRYPTION' => 'ssl',
+    'SMTP_FROM_ADDRESS' => '',
+    'SMTP_FROM_NAME' => '',
+    'SMTP_USER' => '',
+    'SMTP_PASS' => '',
+);
